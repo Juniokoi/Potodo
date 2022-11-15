058c9b59143a83d455c7140b1b933de7cdc0736d
Junio Koi <53125029+Juniokoi@users.noreply.github.com>
2022-11-15 03:26:49 -0300

    fix: Settings needed for config work properly

--- src/app/modules/timer/timer-routing.module.ts
+++ src/app/modules/timer/timer-routing.module.ts
@@ -1,14 +1,15 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-import {TimerComponent} from "./components/timer.component";
-import {ListComponent} from "./components/list/list.component";
-import {PomodoroComponent} from "./components/pomodoro/pomodoro.component";
+import {TimerComponent} from "./timer.component";
+import {ListComponent} from "./modules/list/list.component";
+import {PomodoroComponent} from "./modules/pomodoro/pomodoro.component";
 
 import {ListGuard} from './guards/list.guard';
 import {ListsResolver} from "./guards/lists.resolver";
 import {TimerDeactivateGuard} from "./guards/timer-deactivate.guard";
-import {ConfigComponent} from "./components/config/config.component";
+import {ConfigComponent} from "./modules/config/config.component";
+import {ConfigResolver} from "./guards/config.resolver";
 
 export const routes: Routes = [
     { path: '', component: TimerComponent, children: [
@@ -16,7 +17,10 @@ export const routes: Routes = [
             path: '',
             component: ListComponent,
         },
-        { path: 'config', component: ConfigComponent },
+        { path: 'config',
+            component: ConfigComponent,
+            resolve: {user: ConfigResolver}
+        },
         {
             path: ':id',
             component: PomodoroComponent,
