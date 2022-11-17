b27c1f104e0853c27dc0dc9bd16bbc2f420ed89f
Junio Koi <53125029+Juniokoi@users.noreply.github.com>
2022-11-17 18:50:44 -0300

    Remove home router

--- src/app/modules/core/core-routing.module.ts
+++ src/app/modules/core/core-routing.module.ts
@@ -1,16 +1,13 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {PageNotFoundComponent} from "./components/page-not-found/page-not-found.component";
-import {HomeComponent} from "../home/home.component";
 import {CoreComponent} from "./core.component";
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent},
+    { path: '', redirectTo: 'timer', pathMatch: 'full' },
     { path: 'timer', loadChildren: () =>
             import('../timer/timer.module').then(mod => mod.TimerModule)
     },
-    { path: "**", component: PageNotFoundComponent}
 ];
 
 @NgModule({
